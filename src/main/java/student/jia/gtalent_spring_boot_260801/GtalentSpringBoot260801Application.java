92f3550d9a31c586e93d4198fa0933f4019ce0e2
xxa693852 <xxa693852@gmail.com>
2026-08-04 19:23:26 +0800

    first commit

---
+++ src/main/java/student/jia/gtalent_spring_boot_260801/GtalentSpringBoot260801Application.java
@@ -0,0 +1,13 @@
+package student.jia.gtalent_spring_boot_260801;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtalentSpringBoot260801Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtalentSpringBoot260801Application.class, args);
+	}
+
+}
---
+++ src/test/java/student/jia/gtalent_spring_boot_260801/GtalentSpringBoot260801ApplicationTests.java
@@ -0,0 +1,13 @@
+package student.jia.gtalent_spring_boot_260801;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtalentSpringBoot260801ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
