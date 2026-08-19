966f892742bcca0873d70a9e7ddab3753c10291f
xxa693852 <xxa693852@gmail.com>
2026-08-20 07:01:44 +0800

    Member註冊系統:新增會員

--- src/main/java/student/jia/gtalent_spring_boot_260801/request/MemberRegisterRequest.java
+++ src/main/java/student/jia/gtalent_spring_boot_260801/request/MemberRegisterRequest.java
@@ -3,8 +3,8 @@ package student.jia.gtalent_spring_boot_260801.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank; //不能空白
+import jakarta.validation.constraints.NotNull; //不能為空值
 import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
@@ -15,7 +15,7 @@ import student.jia.gtalent_spring_boot_260801.constant.ResponseMessages;
 @Setter
 public class MemberRegisterRequest {
     
-    @NotBlank(message = ResponseMessages.MEMBER_NAME_REQUIRED)
+    @NotBlank(message = ResponseMessages.MEMBER_NAME_REQUIRED) //不能為空白
     @Size(max = 30, message = ResponseMessages.MEMBER_NAME_MAX)
     private String name;
 
@@ -28,7 +28,7 @@ public class MemberRegisterRequest {
     private String password;
 
     @NotBlank(message = ResponseMessages.MEMBER_CONFIRM_PASSWORD_REQUIRED)
-    private String confirmPassword;
+    private String confirmPassword; //確認密碼
 
     @Email(message = ResponseMessages.MEMBER_EMAIL_INVALID)
     @Size(max = 128, message = ResponseMessages.MEMBER_EMAIL_MAX)
@@ -37,5 +37,5 @@ public class MemberRegisterRequest {
     @NotNull(message = ResponseMessages.MEMBER_GENDER_REQUIRED)
     @Min(value = 0, message = ResponseMessages.MEMBER_GENDER_INVALID)
     @Max(value = 2, message = ResponseMessages.MEMBER_GENDER_INVALID)
-    private Byte gender;
+    private Byte gender; //性別
 }
